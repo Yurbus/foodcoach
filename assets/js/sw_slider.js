52de4cba011da27e1cea8a258685de4781fc4313
Yurbus <frankins636@gmail.com>
2024-06-12 13:50:44 +0300

    add slider

--- assets/js/sw_slider.js
+++ assets/js/sw_slider.js
@@ -1,37 +1,60 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 30,
+
+const swiper = new Swiper(".mySwiper", {
+  speed: 1000,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+},
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
+
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+//     slidesPerView: 4,
+//     spaceBetween: 30,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//       dynamicBullets: true,
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
   
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        },
-        // when window width is >= 480px
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 20
-        },
-        // when window width is >= 640px
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 20
-        }
-      }
-  });
+//     breakpoints: {
+//         // when window width is >= 320px
+//         320: {
+//           slidesPerView: 2,
+//           spaceBetween: 10
+//         },
+//         // when window width is >= 480px
+//         480: {
+//           slidesPerView: 3,
+//           spaceBetween: 20
+//         },
+//         // when window width is >= 640px
+//         768: {
+//           slidesPerView: 4,
+//           spaceBetween: 20
+//         }
+//       }
+//   });
+
+
+
